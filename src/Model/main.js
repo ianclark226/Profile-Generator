a76eb1175103a1f172b379b3263728c47915be5a
Ian Clark <ianclark226@gmail.com>
2020-01-31 12:28:10 -0500

    1/31

--- src/Model/main.js
+++ src/Model/main.js
@@ -1,7 +1,67 @@
 module.exports = class Main {
-    constructor() {}
-
-    run() {
-        console.log("it runs");
+    constructor() {
+        this._teamArray = [];
     }
+
+    async run() {
+    const { teamSize } = await inquirer.prompt([{
+        type: 'input',
+        name: 'teamSize',
+        message: 'Please input size of team',
+        default: 2,
+    }]);
+
+
+    for (let i = 0; i < teamSize; i++) {
+        const response = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message:'Select name',
+
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message:'What is your email',
+        },
+        {
+            type: 'list',
+            Position: 'role',
+            message: 'What is your role',
+            choices: [
+                'Engineer',
+                'Intern',
+                'Manager'
+            ]
+        },
+        {
+            type:'input',
+            name: 'github',
+            message: 'What is your gethub',
+            when: ({ role }) => role === 'Engineer'
+        },
+        {
+            type:'input',
+            name: 'school',
+            message: 'What is your school',
+            when: ({ role }) => role === 'Intern'
+        },
+        {
+            type:'input',
+            name: 'roomNumber',
+            message: 'What is your roomNumber',
+            when: ({ role }) => role === 'Manager'
+        },
+    
+        
+
+    ]);
+
+    this._teamArray.push(response);
 }
+    console.log(this._teamArray);
+
+    }
+        
+    }
